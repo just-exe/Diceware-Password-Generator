18319e134f08fe3b3d3c4b388f1b435ec24cf7b2
Justin McCabe <43969246+Pimpinduck@users.noreply.github.com>
2019-05-01 03:08:54 -0400

    Add files via upload
    Added initial files. Need to fix file traversal.

---
+++ Password.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string number = "";
+    string word = "";
+    vector<string> allNums;
+    vector<string> words;
+    int numWords;
+
+    ifstream in;
+    in.open("1.txt");
+
+    if (in.fail()) {
+        cout << "Could not open file" << endl;
+        return 1;
+    }
+
+    //Gather input for password
+    cout << "Hello! How many words would you like your password to have?" << endl;
+    cin >> numWords;
+
+    //Create numbers to find corresponding words
+    for(int j = 0; j < numWords; j++){
+        for (int i = 0; i < 6; i++) {
+         number += to_string(rand() % 6 + 1);
+        }
+        allNums.push_back(number);
+        cout << allNums.at(j) << endl;
+        number = "";
+    }
+
+    //Seek file for number match, then store word in vector
+    for (int i = 0; i < 6; i++) {
+        in.clear();
+        in.seekg(0, ios::beg);
+        while(getline(in, word)) {
+            //cout << allNums.at(i);
+            if (word.substr(0, 6) == allNums.at(i)) {
+                word = word.substr(6, 9);
+                words.push_back(word);
+                cout << word << endl;
+                continue;
+            }
+        }
+    }
+
+    in.close();
+    return 0;
+}
