e260b9a81532b445379071129b06c42c53b1bad5
Pimpinduck <dachickenmastaog@gmail.com>
2019-06-02 03:20:36 -0400

    Works when compiled with std=c++11 and uses srand for different words every time

--- Password.cpp
+++ Password.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <random>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
@@ -26,28 +27,36 @@ int main() {
     cin >> numWords;
 
     //Create numbers to find corresponding words
+    srand(time(NULL));
     for(int j = 0; j < numWords; j++){
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
          number += to_string(rand() % 6 + 1);
         }
         allNums.push_back(number);
-        cout << allNums.at(j) << endl;
+        //cout << allNums.at(j) << endl;
         number = "";
     }
 
     //Seek file for number match, then store word in vector
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < numWords; i++) {
         in.clear();
         in.seekg(0, ios::beg);
         while(getline(in, word)) {
             //cout << allNums.at(i);
-            if (word.substr(0, 6) == allNums.at(i)) {
+            if (word.substr(0, 5) == allNums.at(i)) {
                 word = word.substr(6, 9);
                 words.push_back(word);
-                cout << word << endl;
+                //cout << word << endl;
                 continue;
             }
         }
+    }
+
+    //Displaying new password to user
+    cout << "Your password is:" << endl;
+
+    for (int i = 0; i < numWords; i++) {
+        cout << words.at(i) << " ";
     }
 
     in.close();
